0f88d4b06392e9db851986278cb6231dd7fe3c87
Viet Le Thanh <vietlt@zigexn.vn>
2018-08-17 13:34:09 +0700

    Add password reset

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class SessionsController < ApplicationController
   def new; end
+
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
